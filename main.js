bf0d6459a2afe8e7cfbfe3e41da294e003a176eb
Yuliya Feland <yuliyakf@gmail.com>
2023-05-09 18:33:50 -0400

    added events to elements

--- main.js
+++ main.js
@@ -8,30 +8,68 @@ let contFive = document.querySelector(".containerFive");
 let contScore= document.querySelector(".containerScore");
 let contLast = document.querySelector(".containerLast");
 let correct= document.querySelectorAll(".correct");
-let incorrect=document.querySelectorAll(".incorrect");
+let correctBtn =document.querySelector("#btnCorrect1")
+let incorrect = document.querySelectorAll(".incorrect")
+
 let answerBtn = document.querySelectorAll(".btn")
 
+
 startButton.addEventListener("click", startQuiz);
-
-
 
 
 
 function startQuiz(){
     contOne.style.display="block";
     contStart.style.display="none";
-
+ 
+    correctAnswer()
    
-    for (let i=0; i < answerBtn.length; i++){
-    answerBtn[i].addEventListener("click", correctAnswer)
 }
 
-}
+    function correctAnswer(){
 
-function correctAnswer(){
-    if(correct ="true"){
-      document.querySelector(".btn").style.backgroundColor="green"  
-    } else if (incorrect = "true"){
-        document.querySelector(".btn").style.backgroundColor="red"
+     incorrect.forEach(incorrect=>{
+        incorrect.addEventListener("click", ()=> incorrect.style.backgroundColor="red");
+     })
+     correct.forEach(correct=>{
+        correct.addEventListener("click", ()=>correct.style.backgroundColor="green");
+    })
+
     }
+
+function questionTwo(){
+    contTwo.style.display="block";
+    contStart.style.display="none";
+    contOne.style.display ="none";
+   
+}
+function questionThree(){
+    contTwo.style.display="none";
+    contStart.style.display="none";
+    contOne.style.display ="none";
+    contThree.style.display = "block";
+}
+function questionFour(){
+    contTwo.style.display="none";
+    contStart.style.display="none";
+    contOne.style.display ="none";
+    contThree.style.display = "none";
+    contFour.style.display="block";
+}
+function questionFive(){
+    contTwo.style.display="none";
+    contStart.style.display="none";
+    contOne.style.display ="none";
+    contThree.style.display = "none";
+    contFour.style.display="none";
+    contFive.style.display ="block";
+}
+function scoreResults(){
+    contTwo.style.display="none";
+    contStart.style.display="none";
+    contOne.style.display ="none";
+    contThree.style.display = "none";
+    contFour.style.display="none";
+    contFive.style.display ="none";
+    contScore.style.display ="block";
 }
